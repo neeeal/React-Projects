e5b1cb4e9c1dcd004f5d091d35c2253f69256f3f
Niil <91337648+neeeal@users.noreply.github.com>
2023-09-06 13:30:47 +0800

    created simple react app

---
+++ Installation/my-react-app/src/index.js
@@ -0,0 +1,38 @@
+import { createRoot } from 'react-dom/client';
+
+const NavigationBar = () => {
+  return <h1>Hello from React!</h1>;
+}
+
+const domNode = document.getElementById('navigation')
+const root = createRoot(domNode);
+root.render(<NavigationBar />)
+
+
+
+// // Clear the existing HTML document
+// document.body.innerHTML = '<div id="app"></div>';
+
+// // Render your React component instead
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, World</h1>);
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
